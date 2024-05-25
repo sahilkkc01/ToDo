ef614f354d1c0de112e0f9ede9731aad76af8c6c
sahilkkc01 <sahilgupta5655@gmail.com>
2024-05-25 16:05:35 +0530

    2

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://to-do-eight-indol.vercel.app/api"
 })
 export default instance
