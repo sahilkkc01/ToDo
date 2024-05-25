7f15bd1cc074eaaac4f933e5fabe65fe7fa87d3d
sahilkkc01 <sahilgupta5655@gmail.com>
2024-05-25 15:52:56 +0530

    1

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import forgotPasswordRouter from "./routes/forgotPassword.js"
 //app config
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 5000
 mongoose.set('strictQuery', true);
 
 //middlewares
